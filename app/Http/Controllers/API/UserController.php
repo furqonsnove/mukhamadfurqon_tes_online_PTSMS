4dcb84de876ef18ca07ffb3170a40f24ddf7822f
Mukhamad Furqon <mfurqon1904@gmail.com>
2022-08-27 10:21:39 +0700

    first commit

---
+++ app/Http/Controllers/API/UserController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Validator;
+
+class UserController extends Controller
+{
+    public function register(Request $request)
+    {
+        //set validation
+        $validator = Validator::make($request->all(), [
+            'name'      => 'required',
+            'email'     => 'required|email|unique:users',
+            'password'  => 'required|min:8'
+        ]);
+
+        //if validation fails
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        //query create user
+        $user = User::create([
+            'name'      => $request->name,
+            'email'     => $request->email,
+            'password'  => bcrypt($request->password)
+        ]);
+
+        //respnse if created
+        if ($user) {
+            return response()->json([
+                'success' => true,
+                'user'    => $user,
+            ], 201);
+        }
+
+        //response insert failed
+        return response()->json([
+            'success' => false,
+        ], 409);
+    }
+
+    public function login(Request $request)
+    {
+        //set validation
+        $validator = Validator::make($request->all(), [
+            'email'     => 'required',
+            'password'  => 'required'
+        ]);
+
+        //if validation fails
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        //get credentials from request
+        $credentials = $request->only('email', 'password');
+
+        //if auth failed
+        if (!$token = auth()->guard('api')->attempt($credentials)) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Email atau Password Anda salah'
+            ], 401);
+        }
+
+        //if auth success
+        return response()->json([
+            'success' => true,
+            'user'    => auth()->guard('api')->user(),
+            'token'   => $token
+        ], 200);
+    }
+}
---
+++ app/Models/Pembelian.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Pembelian extends Model
+{
+    use HasFactory;
+    protected $table = 'tbl_pembelian';
+    protected $primaryKey = 'id';
+    protected $guarded = [];
+
+    public function details()
+    {
+        return $this->hasMany(PembelianDetail::class, 'id', 'pembelian_id');
+    }
+}
